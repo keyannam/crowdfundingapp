12f9d500f05adce9c930e3b13f95abec8f101058
Keyanna Mayfield <11904976+keyannam@users.noreply.github.com>
2023-05-02 07:10:55 -0500

    Load Accounts on the web

---
+++ src/components/Navigation.js
@@ -0,0 +1,22 @@
+import { Container } from 'react-bootstrap';
+
+import Navbar from 'react-bootstrap/Navbar';
+
+import logo from '../logo.png';
+
+const Navigation = () => {
+	return(
+		<Navbar>
+			<img 
+				alt="logo" 
+				src={logo} 
+				width="40" 
+				height="40" 
+				className="d-inline-block align-top mx-3"
+				/>
+			<Navbar.Brand href="#">BLkk ICO Crowdsale</Navbar.Brand>
+		</Navbar>
+	)
+}
+
+export default Navigation;
